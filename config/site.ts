c705b1c90785399186783e17e0eeb6e4513d1f88
caoyi <caoyi@xizi-ai.com>
2024-05-16 10:23:34 +0800

    update: site修改

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,16 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "聪明萝卜的柴犬分享",
+  description: "收集了一些养柴犬的经验, 包括柴犬的挑选, 喂养, 训练, 疾病等",
   mainNav: [
     {
-      title: "Home",
+      title: "柴犬信息",
       href: "/",
+    },
+    {
+      title: "作者信息",
+      href: "/author",
     },
   ],
   links: {
