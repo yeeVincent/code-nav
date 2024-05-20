2c94ee242fba2499b50cdd4b96a06f36b3bb549c
caoyi <caoyi@xizi-ai.com>
2024-05-20 16:12:42 +0800

    add: change display in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,43 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
+import Shiba from "./shiba/page"
+
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="mx-auto max-w-xl py-8">
-        <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
-      </div>
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste into your apps. Free.
-          Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    // <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    //   <div className="mx-auto max-w-xl py-8">
+    //     <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
+    //   </div>
+    //   <div className="flex max-w-[980px] flex-col items-start gap-2">
+    //     <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+    //       Beautifully designed components <br className="hidden sm:inline" />
+    //       built with Radix UI and Tailwind CSS.
+    //     </h1>
+    //     <p className="max-w-[700px] text-lg text-muted-foreground">
+    //       Accessible and customizable components that you can copy and paste into your apps. Free.
+    //       Open Source. And Next.js 13 Ready.
+    //     </p>
+    //   </div>
+    //   <div className="flex gap-4">
+    //     <Link
+    //       href={siteConfig.links.docs}
+    //       target="_blank"
+    //       rel="noreferrer"
+    //       className={buttonVariants()}
+    //     >
+    //       Documentation
+    //     </Link>
+    //     <Link
+    //       target="_blank"
+    //       rel="noreferrer"
+    //       href={siteConfig.links.github}
+    //       className={buttonVariants({ variant: "outline" })}
+    //     >
+    //       GitHub
+    //     </Link>
+    //   </div>
+    // </section>
+    <Shiba></Shiba>
   )
 }
