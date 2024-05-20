e271f3af85ec2a0925c96aa1c673483c5e1a066b
caoyi <caoyi@xizi-ai.com>
2024-05-20 14:01:06 +0800

    add: 拆分目录

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,10 +1,14 @@
 import ListLayout from "@/layout/ListLayout"
 import { allShibas } from "contentlayer/generated"
 
+import { siteConfig } from "@/config/site"
+
 export default function Menu(props: any) {
   return (
     <>
-      <ListLayout shibas={allShibas} title={"柴犬信息"} {...props}></ListLayout>
+      {siteConfig.mainNav.map((navItem) => (
+        <ListLayout shibas={allShibas} title={navItem.title} {...props}></ListLayout>
+      ))}
     </>
   )
 }
