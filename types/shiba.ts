44c95e893cada54ed50cca2907aa183771ec620d
caoyi <caoyi@xizi-ai.com>
2024-05-22 14:57:47 +0800

    add: 准备文章

--- types/shiba.ts
+++ types/shiba.ts
@@ -2,26 +2,29 @@ export enum ShibaTagsEnum {}
 
 export enum ShibaCategoriesEnum {
   select = "select",
-  dailyRoutine = "dailyRoutine",
+  care = "care",
   training = "training",
   health = "health",
   food = "food",
+  dailyRoutine = "dailyRoutine",
   other = "other",
 }
 /** 需要联合类型的原因是 contentLayer会把Enum转化成联合类型, 因此再做一次映射 */
 export type ShibaCategoriesUnionEnum =
   | "select"
-  | "dailyRoutine"
+  | "care"
   | "training"
   | "health"
   | "food"
+  | "dailyRoutine"
   | "other"
 
 export enum ShibaCategoriesNameEnum {
   select = "💰挑选柴犬",
-  dailyRoutine = "🌞狗子日常",
+  care = "🐶柴犬养护",
   training = "🏋️训练柴犬",
-  health = "🐶柴犬健康",
+  health = "🏥柴犬健康",
   food = "🍚柴犬食物",
+  dailyRoutine = "🌞狗子日常",
   other = "其他",
 }
