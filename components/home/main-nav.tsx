ed63cc13b9aa41f44c12c85605cd2756f4819ceb
caoyi <caoyi@xizi-ai.com>
2024-05-23 17:29:27 +0800

    style: 样式微调

--- components/home/main-nav.tsx
+++ components/home/main-nav.tsx
@@ -16,7 +16,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <>
       <div className="hidden gap-6 md:flex md:gap-10">
-        <Link href="/" className="flex items-center space-x-2 ">
+        <Link href="/shiba" className="flex items-center space-x-2 ">
           <Icons.logo className="h-6 w-6" />
           <span className="inline-block font-bold">{siteConfig.name}</span>
         </Link>
--- components/mdx/index.tsx
+++ components/mdx/index.tsx
@@ -3,6 +3,8 @@
 import React from "react"
 import Image from "next/image"
 
+import { cn } from "@/lib/utils"
+
 export const MdxButton = () => {
   return <button onClick={() => alert("Button clicked!")}>Click me!</button>
 }
@@ -13,12 +15,19 @@ interface MdxImageProps {
   width: number
   height: number
   className: string
+  innerClassName: string
 }
 
-export const MdxImage = ({ src, alt, width, height, className }: MdxImageProps) => {
+export const MdxImage = ({ src, alt, width, height, className, innerClassName }: MdxImageProps) => {
   return (
-    <div className={className}>
-      <Image src={src} alt={alt} width={width} height={height} />
+    <div className={cn("w-full md:w-3/5 lg:w-2/3", className)}>
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        className={cn("w-full", innerClassName)}
+      />
     </div>
   )
 }
